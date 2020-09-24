37a204aebc7a19b5c83b0599b14ce1b386121eca
Jillian Gibson <jillian.l.gibson@gmail.com>
2020-09-23 18:10:47 -0700

    add event listener and validate values returned as strings

--- app.js
+++ app.js
@@ -7,5 +7,10 @@ const addTotal = document.getElementById('add-total');
 // initialize state
 
 // set event listeners to update state and DOM
+addButton.addEventListener('click', () => {
+    const value1 = addInput1.value;
+    const value2 = addInput2.value;
+    console.log(value1, value2);
 
+});
   
