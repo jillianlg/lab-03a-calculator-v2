7f81626b950de42711359113bde18e9b8ce3d5be
Jillian Gibson <jillian.l.gibson@gmail.com>
2020-09-24 11:11:31 -0700

    add div function to index.html, add import mul functions on app.js and validate with console.log

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// import functions and grab DOM elements
+// ADDITION import functions and grab DOM elements
 const addInput1 = document.getElementById('add-input-1');
 const addInput2 = document.getElementById('add-input-2');
 const addButton = document.getElementById('add-button');
@@ -6,7 +6,7 @@ const addTotal = document.getElementById('add-total');
 
 // initialize state
 
-// set event listeners to update state and DOM
+// ADDITION set event listeners to update state and DOM
 addButton.addEventListener('click', () => {
     const value1 = addInput1.value;
     const value2 = addInput2.value;
@@ -23,7 +23,7 @@ addButton.addEventListener('click', () => {
 });
   
 
-// import functions and grab DOM elements
+// SUBTRACTION import functions and grab DOM elements
 const subInput1 = document.getElementById('sub-input-1');
 const subInput2 = document.getElementById('sub-input-2');
 const subButton = document.getElementById('sub-button');
@@ -31,7 +31,7 @@ const subTotal = document.getElementById('sub-total');
 
 // initialize state
 
-// set event listeners to update state and DOM
+// SUBTRACTION set event listeners to update state and DOM
 subButton.addEventListener('click', () => {
     const value1 = subInput1.value;
     const value2 = subInput2.value;
@@ -47,15 +47,15 @@ subButton.addEventListener('click', () => {
     subTotal.textContent = sum;
 });
 
-// import functions and grab DOM elements
+// MULTIPLY import functions and grab DOM elements
 const mulInput1 = document.getElementById('mul-input-1');
 const mulInput2 = document.getElementById('mul-input-2');
 const mulButton = document.getElementById('mul-button');
-// const mulTotal = document.getElementById('mul-total');
+const mulTotal = document.getElementById('mul-total');
 
 // initialize state
 
-// set event listeners to update state and DOM
+// MULTIPLY set event listeners to update state and DOM
 mulButton.addEventListener('click', () => {
     const value1 = mulInput1.value;
     const value2 = mulInput2.value;
@@ -67,5 +67,22 @@ mulButton.addEventListener('click', () => {
 
     const sum = input1Number * input2Number;
     console.log(sum);
+
+    mulTotal.textContent = sum;
+});
+
+// DIVIDE import functions and grab DOM elements
+const divInput1 = document.getElementById('div-input-1');
+const divInput2 = document.getElementById('div-input-2');
+const divButton = document.getElementById('div-button');
+// const divTotal = document.getElementById('div-total');
+
+// initialize state
+
+// DIVIDE set event listeners to update state and DOM
+divButton.addEventListener('click', () => {
+    const value1 = divInput1.value;
+    const value2 = divInput2.value;
+    console.log(value1, value2);
 
 });
