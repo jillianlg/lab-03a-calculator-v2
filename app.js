6af09f15a4b37d2605c5f27901c192fb9581924d
Jillian Gibson <jillian.l.gibson@gmail.com>
2020-09-23 18:05:05 -0700

    add add function to index.html and import functions on app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+// import functions and grab DOM elements
+const addInput1 = document.getElementById('add-input-1');
+const addInput2 = document.getElementById('add-input-2');
+const addButton = document.getElementById('add-button');
+const addTotal = document.getElementById('add-total');
+
+// initialize state
+
+// set event listeners to update state and DOM
+
+  
