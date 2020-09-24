4688de70376296f50e925d7ce2fe8005bab7546d
Jillian Gibson <jillian.l.gibson@gmail.com>
2020-09-24 10:55:31 -0700

    add sub function to index.html, add import functions on app.js and validate with console.log

--- app.js
+++ app.js
@@ -22,3 +22,20 @@ addButton.addEventListener('click', () => {
     addTotal.textContent = sum;
 });
   
+
+// import functions and grab DOM elements
+const subInput1 = document.getElementById('sub-input-1');
+const subInput2 = document.getElementById('sub-input-2');
+const subButton = document.getElementById('sub-button');
+// const subTotal = document.getElementById('sub-total');
+
+// initialize state
+
+// set event listeners to update state and DOM
+subButton.addEventListener('click', () => {
+    const value1 = subInput1.value;
+    const value2 = subInput2.value;
+    console.log(value1, value2);
+
+
+});
