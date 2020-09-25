d1779a83ff3e8f7bb81911a5812ce71812a0e67c
Jillian Gibson <jillian.l.gibson@gmail.com>
2020-09-24 18:42:44 -0700

    add test and import for mul functions on example.test.js

--- test/example.test.js
+++ test/example.test.js
@@ -1,5 +1,5 @@
 // IMPORT MODULES under test here:
-import { addTwoNumbers, subTwoNumbers } from '../mathUtils.js';
+import { addTwoNumbers, subTwoNumbers, mulTwoNumbers } from '../mathUtils.js';
 
 const test = QUnit.test;
 
@@ -24,16 +24,27 @@ test('should return 7 if passed 3 and 4', (expect) => {
 // SUBTRACTION TEST
 test('should return 4 if passed 9 and 5', (expect) => {
     //Arrange
-    // Set up your arguments and expectations
     const expected = 4;
     const firstNumber = 9;
     const secondNumber = 5;
     
     //Act 
-    // Call the function you're testing and set the result to a const
     const actual = subTwoNumbers(firstNumber, secondNumber);
 
     //Expect
-    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+
+// Multiply TEST
+test('should return 20 if passed 4 and 5', (expect) => {
+    //Arrange
+    const expected = 20;
+    const firstNumber = 4;
+    const secondNumber = 5;
+    
+    //Act 
+    const actual = mulTwoNumbers(firstNumber, secondNumber);
+
+    //Expect
     expect.equal(actual, expected);
 });
