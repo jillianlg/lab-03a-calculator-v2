d2cc917173718da3a576afc8497294c0e0a1252f
Jillian Gibson <jillian.l.gibson@gmail.com>
2020-09-24 19:28:14 -0700

    add 2nd set of test per function, ran test in server to validate: add function -test passed, subtract function - test passed, multiply functions - test passed, divide functions - test passed

--- test/example.test.js
+++ test/example.test.js
@@ -21,6 +21,22 @@ test('should return 7 if passed 3 and 4', (expect) => {
     expect.equal(actual, expected);
 });
 
+test('should return 10 if passed 6 and 4', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = 10;
+    const firstNumber = 6;
+    const secondNumber = 4;
+    
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = addTwoNumbers(firstNumber, secondNumber);
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+
 // SUBTRACTION TEST
 test('should return 4 if passed 9 and 5', (expect) => {
     //Arrange
@@ -35,11 +51,37 @@ test('should return 4 if passed 9 and 5', (expect) => {
     expect.equal(actual, expected);
 });
 
+test('should return 5 if passed 10 and 5', (expect) => {
+    //Arrange
+    const expected = 5;
+    const firstNumber = 10;
+    const secondNumber = 5;
+    
+    //Act 
+    const actual = subTwoNumbers(firstNumber, secondNumber);
+
+    //Expect
+    expect.equal(actual, expected);
+});
+
 // Multiply TEST
-test('should return 20 if passed 4 and 5', (expect) => {
+test('should return 15 if passed 3 and 5', (expect) => {
     //Arrange
     const expected = 15;
     const firstNumber = 3;
+    const secondNumber = 5;
+    
+    //Act 
+    const actual = mulTwoNumbers(firstNumber, secondNumber);
+
+    //Expect
+    expect.equal(actual, expected);
+});
+
+test('should return 20 if passed 4 and 5', (expect) => {
+    //Arrange
+    const expected = 20;
+    const firstNumber = 4;
     const secondNumber = 5;
     
     //Act 
@@ -55,6 +97,19 @@ test('should return 4 if passed 12 and 3', (expect) => {
     const expected = 4;
     const firstNumber = 12;
     const secondNumber = 3;
+    
+    //Act 
+    const actual = divTwoNumbers(firstNumber, secondNumber);
+
+    //Expect
+    expect.equal(actual, expected);
+});
+
+test('should return 5 if passed 25 and 5', (expect) => {
+    //Arrange
+    const expected = 5;
+    const firstNumber = 25;
+    const secondNumber = 5;
     
     //Act 
     const actual = divTwoNumbers(firstNumber, secondNumber);
